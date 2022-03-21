2827f0b82789d0845952435fd620a2f784d49c7f
unknown <chrystmark2020@mail.ugm.ac.id>
2022-03-21 20:50:00 +0700

    Assignment 5

---
+++ README.md
@@ -0,0 +1,1 @@
+This API uses the get method to get the sentiment analysis of the provided sentence.
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from model import NLPModel
 import numpy as np
 import pickle
-8from flask import Flask, request
+from flask import Flask, request
 
 app = Flask(__name__)
 # api = Api(app)
