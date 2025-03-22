456f9d2a4c7dbb26d7046a8d721088003f265cc4
Markkreel <chrystmark2020@mail.ugm.ac.id>
2025-03-23 01:41:18 +0700

    refactor(build_model.py): reorganize imports for better readability

--- build_model.py
+++ build_model.py
@@ -1,9 +1,6 @@
-from model import NLPModel
 import pandas as pd
-
-# import numpy as np
-# import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from model import NLPModel
 
 
 def build_model():
