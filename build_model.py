61c505ff0b6f2067a4fe048a261a18829387e699
Markkreel <chrystmark2020@mail.ugm.ac.id>
2025-03-23 01:34:27 +0700

    style: standardize string quotes and improve code readability

--- build_model.py
+++ build_model.py
@@ -1,5 +1,6 @@
 from model import NLPModel
 import pandas as pd
+
 # import numpy as np
 # import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -10,25 +11,24 @@ def build_model():
 
     # filename = os.path.join(
     #     os.path.dirname(__file__), 'chalicelib', 'all/train.tsv')
-    with open('../sentiment_data/train.tsv') as f:
-        data = pd.read_csv(f, sep='\t')
+    with open("../sentiment_data/train.tsv") as f:
+        data = pd.read_csv(f, sep="\t")
 
-    pos_neg = data[(data['Sentiment'] == 0) | (data['Sentiment'] == 4)]
+    pos_neg = data[(data["Sentiment"] == 0) | (data["Sentiment"] == 4)]
 
-    pos_neg['Binary'] = pos_neg.apply(
-        lambda x: 0 if x['Sentiment'] == 0 else 1, axis=1)
+    pos_neg["Binary"] = pos_neg.apply(lambda x: 0 if x["Sentiment"] == 0 else 1, axis=1)
 
-    model.vectorizer_fit(pos_neg.loc[:, 'Phrase'])
-    print('Vectorizer fit complete')
+    model.vectorizer_fit(pos_neg.loc[:, "Phrase"])
+    print("Vectorizer fit complete")
 
-    X = model.vectorizer_transform(pos_neg.loc[:, 'Phrase'])
-    print('Vectorizer transform complete')
-    y = pos_neg.loc[:, 'Binary']
+    X = model.vectorizer_transform(pos_neg.loc[:, "Phrase"])
+    print("Vectorizer transform complete")
+    y = pos_neg.loc[:, "Binary"]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     model.train(X_train, y_train)
-    print('Model training complete')
+    print("Model training complete")
 
     model.pickle_clf()
     model.pickle_vectorizer()
